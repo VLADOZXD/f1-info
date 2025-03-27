41bf6efad54487acd7f415e7bbafa2fab1ad9f18
EtoZheVladoZ <vlad-ozad@ukr.net>
2025-03-27 23:42:31 +0200

    add api for standings

--- api/index.py
+++ api/index.py
@@ -66,3 +66,50 @@ async def get_race_podium(year: int, round: int):
         return {"podium": race_podium}
     except Exception as e:
         return {"error": str(e)}
+
+@app.get("/api/py/driver-standings/{year}")
+async def get_driver_standings(year: int):
+    try:
+        standings_data = ergast.get_driver_standings(year, result_type='raw')[0]
+
+        round = standings_data["round"]
+
+        driver_standings = standings_data["DriverStandings"]
+
+        standings = []
+        for driver in driver_standings:
+            team_name = driver['Constructors'][-1]['name']
+
+            driver_data = {
+                "position": driver["positionText"],
+                "points": driver["points"],
+                "driver_name": f"{driver['Driver']['givenName']} {driver['Driver']['familyName']}",
+                "team_name": team_name,
+                "team_color": get_team_color(year, round, team_name) if year > 2017 else "#b8b8b8"
+            }
+            standings.append(driver_data)
+        return {"driver_standings": standings}
+    except Exception as e:
+        return {"error": str(e)}
+        
+@app.get("/api/py/constructor-standings/{year}")
+async def get_constructor_standings(year: int):
+    try:
+        standings_data = ergast.get_constructor_standings(year, result_type='raw')[0]
+
+        constructor_standings = standings_data["ConstructorStandings"]
+
+        standings = []
+        for constructor in constructor_standings:
+            construcor_name = constructor['Constructor']['name']
+
+            constructor_data = {
+                "position": constructor["positionText"],
+                "points": constructor["points"],
+                "construcor_name": construcor_name,
+                "team_color": get_team_color(year, 1, construcor_name) if year > 2017 else "#b8b8b8"
+            }
+            standings.append(constructor_data)
+        return {"constructor_standing": standings}
+    except Exception as e:
+        return {"error": str(e)}  
--- api/utils/get_team_color.py
+++ api/utils/get_team_color.py
@@ -1,10 +1,11 @@
 import fastf1
+import fastf1.plotting
 
 def get_team_color(year: int, round: int, constructor_name: str):
     try:
-        session = fastf1.get_session(year, round, 5)
+        session = fastf1.get_session(year, round, "R")
 
-        team_color = fastf1.plotting.get_team_color(constructor_name, session)
+        team_color = fastf1.plotting.get_team_color(constructor_name, session, colormap="official")
        
         return team_color
     except Exception as e:
